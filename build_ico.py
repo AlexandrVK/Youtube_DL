6f76bb0fb21e9357d6e31ae4f2c6c447428c69e1
AlexandrVK <alexandrvk@gmail.com>
2025-07-07 18:49:29 +0300

    Добавлена иконка

---
+++ build_ico.py
@@ -0,0 +1,11 @@
+from PIL import Image
+
+input_png = "icon.png"  # Ваш PNG 1024x1024
+output_ico = "icon.ico"
+
+sizes = [16, 32, 48, 64, 128, 256]
+
+img = Image.open(input_png)
+img.save(output_ico, format="ICO", sizes=[(size, size) for size in sizes])
+
+print(f"Создан файл {output_ico} с размерами: {sizes}")
